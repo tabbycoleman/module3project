fbeff5862cdf07e969bd31e3fac61426c83e620f
Kate Coleman <bearyk8@gmail.com>
2024-06-04 09:36:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,23 +13,42 @@ const endorsementsInDB = ref(database, "endorsements")
 const publishBtn = document.getElementById("publish-btn")
 const endorsement = document.getElementById("textbox")
 const list = document.getElementById("feedback-database")
+const fromField = document.getElementById("from")
+const toField = document.getElementById("to")
 
 publishBtn.addEventListener("click", function(){ 
     
     let endorsementValue = endorsement.value
+    let fromValue = fromField.value
+    let toValue = toField.value
     
     if(endorsementValue == 0){
         
         document.getElementById("textbox").style.border = "1px solid red";
         
+        
     }
+    
     else
+    
     {
         document.getElementById("textbox").style.border = "none";
         push( endorsementsInDB, endorsementValue )
-        list.innerHTML += `<li>${endorsementValue}</li>`
+        list.innerHTML += `
+         <li>
+            <span class="bold">To ${toValue}</span>
+            <span class= "paragraph"> 
+                <br> <br> 
+                ${endorsementValue} 
+                <br> <br> 
+            </span>
+            <span class="bold">From ${fromValue}</span>
+         </li> 
+        `
         
     }
+    
 })
-
+    
+      
 
